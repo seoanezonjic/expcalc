e192b95271d940c0c1b75f64b8bc1435200af3aa
seoanezonjic <seoanezonjic@hotmail.com>
2022-02-03 16:31:37 +0100

    Rfactor hash to matrix methods

--- lib/expcalc/numo_expansion.rb
+++ lib/expcalc/numo_expansion.rb
@@ -7,57 +7,60 @@ require 'time'
 require 'npy'
 #require 'pp'
 
-# TODO: Ver de llamar a Netanalyzer aqui para usar su expansion del Numo Narray para meter metodos para tranformar un hash en matriz y viceversa. 
-# Tb meterle metodos para escribir y lerr matrices con npy
-def hash2bin_matrix(hash)
-  x_names_indx = get_hash_values_idx(hash)
-  y_names = hash.keys
-  x_names = x_names_indx.keys
-   # row (y), cols (x)
-  matrix = Numo::DFloat.zeros(hash.length, x_names.length)
-  i = 0
-  hash.each do |id, items|
-    items.each do |item_id|
-      matrix[i, x_names_indx[item_id]] = 1
-    end
-    i += 1
-  end
-  return matrix, y_names, x_names
-end
+# Tb meter leer matrices con npy
+class Hash
+	def get_hash_values_idx
+	  x_names_indx = {}
+	  i = 0
+	  self.each do |k, values|
+	    values.each do |val_id|
+	      query = x_names_indx[val_id]
+	      if query.nil?
+	        x_names_indx[val_id] = i
+	        i += 1
+	      end
+	    end
+	  end
+	  return x_names_indx
+	end
 
-def get_hash_values_idx(hash)
-  x_names_indx = {}
-  i = 0
-  hash.each do |k, items|
-    items.each do |item_id|
-      query = x_names_indx[item_id]
-      if query.nil?
-        x_names_indx[item_id] = i
-        i += 1
-      end
-    end
-  end
-  return x_names_indx
-end
+	def to_bmatrix 
+	  x_names_indx = self.get_hash_values_idx
+	  y_names = self.keys
+	  x_names = x_names_indx.keys
+	   # row (y), cols (x)
+	  matrix = Numo::DFloat.zeros(self.length, x_names.length)
+	  i = 0
+	  self.each do |id, items|
+	    items.each do |item_id|
+	      matrix[i, x_names_indx[item_id]] = 1
+	    end
+	    i += 1
+	  end
+	  return matrix, y_names, x_names
+	end
 
-def hash2weighted_matrix(profiles_similarity)
-  element_names = profiles_similarity.keys
-  matrix = Numo::DFloat.zeros(element_names.length, element_names.length)
-  i = 0
-  profiles_similarity.each do |elementA, relations|
-    element_names.each_with_index do |elementB, j|
-      if elementA != elementB
-        query = relations[elementB]
-        if !query.nil?
-          matrix[i, j] = query
-        else
-          matrix[i, j] = profiles_similarity[elementB][elementA]
-        end
-      end
-    end
-    i += 1
-  end
-  return matrix, element_names
+
+	# TODO: Only works if the resultin matrix will be squared. Replace implementacion taking into account to_bmatrix and its output
+	def to_wmatrix
+	  element_names = self.keys
+	  matrix = Numo::DFloat.zeros(element_names.length, element_names.length)
+	  i = 0
+	  self.each do |elementA, relations|
+	    element_names.each_with_index do |elementB, j|
+	      if elementA != elementB
+	        query = relations[elementB]
+	        if !query.nil?
+	          matrix[i, j] = query
+	        else
+	          matrix[i, j] = self[elementB][elementA]
+	        end
+	      end
+	    end
+	    i += 1
+	  end
+	  return matrix, element_names
+	end
 end
 
 module Numo

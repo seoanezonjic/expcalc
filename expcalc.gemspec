dbdf63f90c3e3230ef8c5fe9dc863bce04cc90f0
seoanezonjic <seoanezonjic@hotmail.com>
2022-02-03 13:29:33 +0100

    Received matrix_manipulation methods from PETS

--- expcalc.gemspec
+++ expcalc.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{\Abin/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "cmath", ">= 1.0.0"
   spec.add_dependency "numo-linalg", ">= 0.1.5"
   spec.add_dependency "numo-narray", ">= 0.9.1.9"
   spec.add_dependency "pp", ">= 0.1.0"
--- lib/expcalc/numo_expansion.rb
+++ lib/expcalc/numo_expansion.rb
@@ -4,10 +4,70 @@ require 'numo/narray'
 require 'numo/linalg'
 require 'cmath'
 require 'time'
+require 'npy'
 #require 'pp'
+
+# TODO: Ver de llamar a Netanalyzer aqui para usar su expansion del Numo Narray para meter metodos para tranformar un hash en matriz y viceversa. 
+# Tb meterle metodos para escribir y lerr matrices con npy
+def hash2bin_matrix(hash)
+  x_names_indx = get_hash_values_idx(hash)
+  y_names = hash.keys
+  x_names = x_names_indx.keys
+   # row (y), cols (x)
+  matrix = Numo::DFloat.zeros(hash.length, x_names.length)
+  i = 0
+  hash.each do |id, items|
+    items.each do |item_id|
+      matrix[i, x_names_indx[item_id]] = 1
+    end
+    i += 1
+  end
+  return matrix, y_names, x_names
+end
+
+def get_hash_values_idx(hash)
+  x_names_indx = {}
+  i = 0
+  hash.each do |k, items|
+    items.each do |item_id|
+      query = x_names_indx[item_id]
+      if query.nil?
+        x_names_indx[item_id] = i
+        i += 1
+      end
+    end
+  end
+  return x_names_indx
+end
+
+def hash2weighted_matrix(profiles_similarity)
+  element_names = profiles_similarity.keys
+  matrix = Numo::DFloat.zeros(element_names.length, element_names.length)
+  i = 0
+  profiles_similarity.each do |elementA, relations|
+    element_names.each_with_index do |elementB, j|
+      if elementA != elementB
+        query = relations[elementB]
+        if !query.nil?
+          matrix[i, j] = query
+        else
+          matrix[i, j] = profiles_similarity[elementB][elementA]
+        end
+      end
+    end
+    i += 1
+  end
+  return matrix, element_names
+end
 
 module Numo
 	class NArray
+		def save(matrix_filename, x_axis_names, x_axis_file, y_axis_names=nil, y_axis_file=nil)
+		  File.open(x_axis_file, 'w'){|f| f.print x_axis_names.join("\n") }
+		  File.open(y_axis_file, 'w'){|f| f.print y_axis_names.join("\n") } if !y_axis_names.nil?
+		  Npy.save(matrix_filename, self)
+		end
+
 
 		def div(second_mat) #Matrix division A/B => A.dot(B.pinv) #https://stackoverflow.com/questions/49225693/matlab-matrix-division-into-python
 			return self.dot(second_mat.pinv)

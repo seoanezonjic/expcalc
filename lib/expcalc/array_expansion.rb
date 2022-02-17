affebfc86325a56335568c51883c24aa87774f77
seoanezonjic <seoanezonjic@hotmail.com>
2022-02-17 12:37:52 +0100

    Added get_quantile method from semtools

--- lib/expcalc/array_expansion.rb
+++ lib/expcalc/array_expansion.rb
@@ -10,4 +10,15 @@ class Array
 		return Math.sqrt(variance)
 	end
 
+	def get_quantiles(position=0.5)
+	  self.sort!
+	  n_items = self.size
+	  quantile_coor = n_items * position - 1
+	  if n_items % 2 == 0
+	    quantile_value = (self[quantile_coor.to_i] + self[quantile_coor.to_i + 1]).fdiv(2)   
+	  else
+	    quantile_value = self[quantile_coor.ceil]
+	  end
+	  return quantile_value
+	end
 end
